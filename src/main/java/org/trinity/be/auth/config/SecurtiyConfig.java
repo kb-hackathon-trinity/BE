f434164fe52331617ce6fe8abeeec6bbcbe764b3
wxxhyeong <wxxhyeong@gmail.com>
2024-11-22 09:04:29 +0900

    fix: 오류 수정

--- src/main/java/org/trinity/be/auth/config/SecurtiyConfig.java
+++ src/main/java/org/trinity/be/auth/config/SecurtiyConfig.java
@@ -2,17 +2,60 @@ package org.trinity.be.auth.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.trinity.be.auth.CustomAuthenticationFilter;
 
 @EnableWebSecurity
 @Log4j
 @Configuration
 @ComponentScan(basePackages = {"org.trinity.be"})
 @RequiredArgsConstructor
-public class SecurtiyConfig extends WebSecurityConfigurerAdapter{
+public class SecurtiyConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomAuthenticationFilter customAuthenticationFilter;
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .cors()
+//                .and()
+//                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling()
+//                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+//
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOriginPattern("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        config.addExposedHeader("Authorization");
+//        config.addExposedHeader("Refresh-Token");
+//        return new CorsFilter(source);
+//    }
 }
